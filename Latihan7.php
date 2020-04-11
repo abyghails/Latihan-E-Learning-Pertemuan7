dff931e5fa14e1804e51259daa20f61eff8e2bc1
abyghails <abyghail10@gmail.com>
2020-04-11 22:36:52 +0700

    Latihan Pertemuan 7

---
+++ Latihan7.php
@@ -0,0 +1,15 @@
+<HTML>
+
+<HEAD>
+    <TITLE> Penggunaan Split </TITLE>
+</HEAD>
+
+<BODY>
+    <?php
+    $kata = "1234";
+    $arr = str_split($kata);
+    echo $arr[0];
+    ?>
+</BODY>
+
+</HTML>
